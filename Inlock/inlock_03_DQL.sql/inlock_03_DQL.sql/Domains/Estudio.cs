15b143a45e3cfe4d4074583f64bd3c0dd8a715d5
Derlis Georges <derlis.cano@senaisp.edu.br>
2021-04-30 11:02:50 -0300

    inlock quase finalizado

---
+++ Inlock/inlock_03_DQL.sql/inlock_03_DQL.sql/Domains/Estudio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace inlock_03_DQL.sql.Domains
+{
+    public partial class Estudio
+    {
+        public Estudio()
+        {
+            Jogos = new HashSet<Jogo>();
+        }
+
+        public int IdEstudio { get; set; }
+        public string NomeEstudio { get; set; }
+
+        public virtual ICollection<Jogo> Jogos { get; set; }
+    }
+}
